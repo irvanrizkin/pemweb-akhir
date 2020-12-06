f59317a343ba31fda1bc421ea28c26061430cc00
Aldi Himawan <aldihimawan88@gmail.com>
2020-12-06 11:11:03 +0700

    Add homePage, AdminPanel, SignIn, transactionHistory, and style

---
+++ application/views/homePage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,500;0,700;1,500&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,600&family=Ubuntu:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
+
+    <!-- Tailwinds -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="/assets/style/tailwind.css" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/assets/style/style.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+    <title>Home Page</title>
+</head>
+
+<body class="h-screen tracking-normal text-gray-900">
+    <div class="h-screen pb-14 bg-right bg-cover">
+        <div class="container pt-24 md:pt-48 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+            <!-- Left Col -->
+            <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+                <h1 class="my-4 text-3xl md:text-5xl text-blue-700 font-bold leading-tight text-center md:text-left">
+                    Welcome
+                </h1>
+                <p class="leading-normal text-base md:text-2xl mb-6 text-center md:text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div class="flex w-full space-x-10 justify-center md:justify-start pb-2 lg:pb-0">
+                    <a href="#" class="bg-blue-600 text-white font-bold text-lg hover:bg-blue-700 p-2 mt-8 rounded-full px-4">
+                        Signin</a>
+                    <a href="#" class="bg-gray-200 font-bold text-lg hover:bg-gray-300 p-2 mt-8 rounded-full px-4">
+                        Transaction</a>
+                </div>
+            </div>
+
+            <!-- Right Col -->
+            <div class="w-full xl:w-3/5 py-6 overflow-y-hidden">
+                <img class="w-5/6 mx-auto lg:mr-0" src="">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
